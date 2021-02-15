711ff71382c23484f20d2fdfd26c1e0dfe1695ac
Suvi <suvi.s.vaisanen@gmail.com>
2021-02-15 10:03:41 +0100

    add changes to test-palindrome

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-//comment
-
 module.exports = Phrase;
 
 // Adds 'reverse' to all strings 
@@ -8,45 +6,25 @@ String.prototype.reverse = function() {
     return Array.from(this).reverse().join(""); //array from returns a emojis as well
 }
 
-String.prototype.blank = function () {
-    return !!(this.match(/^\s*$/g));
-    }
-
-Array.prototype.last = function() {
-    return this.slice(-1);
-}
-
 // Defines Phrase Object 
 
 function Phrase(content) {
     this.content = content;
 
-    this.processor = function(string) {
-        return string.toLowerCase();
-}
-
     // Returns processed content
-    this.processedContent = function processedContent(){
-        return this.processor(this.content);
+    this.processedContent = function processedContent() {
+        return this.content.toLowerCase();
+    }
+
+    // Returns the letters in the content
+    this.letters = function letters() {
+        return this.content; // stub return value
     }
 
     // Returns a true palindrome or false otherwise.
 
     this.palindrome = function palindrome() {
-        return this.processedContent() === this.processedContent();
+        return this.processedContent() === this.processedContent().reverse();
     }
 }
-
-// Defines Translated Phrase
-function TranslatedPhrase(content, translation) {
-    this.content = content;
-    this.translation = translation;
-
-    // Returns translation processed for palindrome testing
-    this.processedContent = function processedContent() {
-        return this.processor(this.translation);
-    }
-}
- 
-TranslatedPhrase.prototype = new Phrase();
 
